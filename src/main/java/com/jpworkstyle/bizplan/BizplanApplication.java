72472b61d53c817d4a04500d4479e82dde14a7c6
Hidenori Miyamoto <twentysecond.hide@gmail.com>
2019-11-10 18:00:28 +0900

    initial commit

---
+++ src/main/java/com/jpworkstyle/bizplan/BizplanApplication.java
@@ -0,0 +1,13 @@
+package com.jpworkstyle.bizplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BizplanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BizplanApplication.class, args);
+	}
+
+}
